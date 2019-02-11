c8cb91072a47126b78a142333e43b479dac6e2f6
Tomek Stacharski <twtstacharski@gmail.com>
2019-02-11 22:55:12 +0800

    Added destroy method

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -20,6 +20,8 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
+    @cocktail = Cocktail.find(@dose.cocktail_id)
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'cocktails#index'
+  resources :doses, only: [:destroy]
   resources :cocktails, except: [:edit, :update ] do
-    resources :doses, only: [:new, :create, :delete]
+    resources :doses, only: [:new, :create, :destroy]
   end
 end
