446ac7c141bc312e6b27f3270198670742ee84d6
Tomek Stacharski <twtstacharski@gmail.com>
2019-02-12 15:43:14 +0800

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Cocktail.destroy_all()
-Ingredient.destroy_all()
+Cocktail.destroy_all
+Ingredient.destroy_all
 
 Ingredient.create(name: 'lemon')
 Ingredient.create(name: 'ice')
@@ -21,58 +21,43 @@ Ingredient.create(name: 'bitters')
 Ingredient.create(name: 'whiskey')
 Ingredient.create(name: 'soda water')
 
-screwdriver = Cocktail.new(
-  name: 'screwdriver'
-)
-screwdriver.remote_photo_url = 'https://images.unsplash.com/photo-1543363950-c78545037afc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80'
-screwdriver.save
+IMAGES = [
+  'https://images.unsplash.com/photo-1531387367216-681093c0279b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60',
+  'https://images.unsplash.com/photo-1512103865222-dcf9531c9961?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60',
+  'https://images.unsplash.com/photo-1509669803555-fd5edd8d5a41?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60',
+  'https://images.unsplash.com/photo-1506802913710-40e2e66339c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60',
+  'https://images.unsplash.com/photo-1486947799489-3fabdd7d32a6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60',
+  'https://images.unsplash.com/photo-1532509279463-999d8e6554e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60',
+  'https://images.unsplash.com/photo-1514359652734-6205dd477a1e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60',
+  'https://images.unsplash.com/photo-1486428263684-28ec9e4f2584?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60',
+  'https://images.unsplash.com/photo-1461823385004-d7660947a7c0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60',
+  'https://images.unsplash.com/photo-1522224050657-33e363782179?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60',
+  'https://images.unsplash.com/photo-1470338745628-171cf53de3a8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=60',
+  'https://images.unsplash.com/photo-1526892210127-f6e3add154ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1400&q=60'
+]
 
-Cocktail.create(
-  name: 'manhattan',
-  photo: 'https://images.unsplash.com/photo-1470337458703-46ad1756a187?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60'
-)
-Cocktail.create(
-  name: 'tom collins',
-  photo: 'https://images.unsplash.com/photo-1536935338788-846bb9981813?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60'
-)
-Cocktail.create(
-  name: 'singapore sling',
-  photo: 'https://images.unsplash.com/photo-1527271982979-83fea3eb3582?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60'
-)
-Cocktail.create(
-  name: 'whiskey sours',
-  photo: 'https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60'
-)
-Cocktail.create(
-  name: 'cosmopolitan',
-  photo: 'https://images.unsplash.com/photo-1527091755570-1daada653420?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9'
-)
-Cocktail.create(
-  name: 'cheeky monkey',
-  photo: 'https://images.unsplash.com/photo-1512103865222-dcf9531c9961?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60'
-)
-Cocktail.create(
-  name: 'gin and tonic',
-  photo: 'https://images.unsplash.com/photo-1481391564276-da6fcacaee31?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60'
-)
-Cocktail.create(
-  name: 'vodka soda lime',
-  photo: 'https://images.unsplash.com/photo-1532509279463-999d8e6554e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60'
-)
-Cocktail.create(
-  name: 'dark and stormy',
-  photo: 'https://images.unsplash.com/photo-1470338745628-171cf53de3a8?ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60'
-)
-Cocktail.create(
-  name: 'moscow mule',
-  photo: 'https://images.unsplash.com/photo-1523286853180-6cc0906b101d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=60'
-)
+COCKTAILS = [
+  'screwdriver',
+  'manhattan',
+  'tom collins',
+  'singapore sling',
+  'whiskey sours',
+  'cosmopolitan',
+  'cheeky monkey',
+  'gin and tonic',
+  'vodka soda lime',
+  'dark and stormy',
+  'moscow mule',
+  'mojito'
+]
 
-
-
-
-
-
+images.each_with_index do |photo, index|
+  cocktail = Cocktail.new(
+    name: COCKTAILS[index]
+  )
+  cocktail.remote_photo_url = photo
+  cocktail.save
+end
 
 # mojito = Cocktail.create(name: "mojito")
 # caiprinha = Cocktail.create(name: "caiprinha")
